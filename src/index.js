cba81ccd020d8421d3fc2b29d589ba1d1f316c6a
bideeen <b.abideen@yahoo.com>
2022-05-10 18:11:14 +0100

    elcaro 1.0

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css'
+import Elcaro from './Elcaro'
+
+
+ReactDOM.render(
+  <Elcaro />,
+  document.getElementById('root')
+);
+
